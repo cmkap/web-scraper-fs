4bca0e506d24633d2cb01d76c41165017de7887a
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-08-03 08:29:41 +0100

    scraper updated: multiple names and odds are selected using xpath

--- server/scrapers.js
+++ server/scrapers.js
@@ -5,23 +5,29 @@ async function scrapeEvent(url) {
     const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: 'networkidle2',
-      });
+    });
+    const rows = await page.$x('//*[@id="main-view"]/ms-antepost-fixture/section/ms-antepost-market/section/div/ms-antepost-participants/ms-fixture-pick')
     
+    const riders = []
 
-    const [el] = await page.$x('//*[@id="main-view"]/ms-antepost-fixture/section/ms-antepost-market/section/div/ms-antepost-participants/ms-fixture-pick[1]/div/span[2]')
-    console.log(el)
-    const text = await el.getProperty('textContent');
-    const name = await text.jsonValue();
+    for (let i = 0; i < rows.length; i++) {
+        const [el] = await page.$x(`//*[@id="main-view"]/ms-antepost-fixture/section/ms-antepost-market/section/div/ms-antepost-participants/ms-fixture-pick[${i+1}]/div/span[2]`)
+        const text = await el.getProperty('textContent');
+        const name = await text.jsonValue();
+    
+        const [el2] = await page.$x(`//*[@id="main-view"]/ms-antepost-fixture/section/ms-antepost-market/section/div/ms-antepost-participants/ms-fixture-pick[${i+1}]/div/span[1]`)
+        const text2 = await el2.getProperty('textContent');
+        const odds = await text2.jsonValue();
 
-    const [el2] = await page.$x('//*[@id="main-view"]/ms-antepost-fixture/section/ms-antepost-market/section/div/ms-antepost-participants/ms-fixture-pick[1]/div/span[1]')
-    const text2 = await el2.getProperty('textContent');
-    const odds = await text2.jsonValue();
+        riders.push({name, odds})
+    }
+  
 
     browser.close();
 
-    console.log({name, odds})
+    console.log(riders)
 
-    return {name, odds}
+    return riders
 
 }
 
